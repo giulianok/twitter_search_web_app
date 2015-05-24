aaed92d945f2cb398f46a47e2e23092973bdee81
isBatak <ivicabatinic123@gmail.com>
2015-05-25 01:26:02 +0200

    io connects is now connecting to remote domain

--- public/app/components/search/searchService.js
+++ public/app/components/search/searchService.js
@@ -1,6 +1,7 @@
 app.
 factory('SearchService', ['socketFactory', function(socketFactory) {
-        var myIoSocket = io.connect('http://localhost:3000');
+        //var myIoSocket = io.connect('http://localhost:3000');
+        var myIoSocket = io.connect('http://nodejs-igrica.rhcloud.com/:8000');
 
         mySocket = socketFactory({
             ioSocket: myIoSocket
