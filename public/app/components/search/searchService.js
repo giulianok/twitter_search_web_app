795a8f19235e8e3fed12cd682e4fb6fedff5ecc2
isBatak <ivicabatinic123@gmail.com>
2015-05-25 01:39:44 +0200

    another change in io connect method doh

--- public/app/components/search/searchService.js
+++ public/app/components/search/searchService.js
@@ -1,7 +1,7 @@
 app.
 factory('SearchService', ['socketFactory', function(socketFactory) {
         //var myIoSocket = io.connect('http://localhost:3000');
-        var myIoSocket = io.connect('ws://nodejs-igrica.rhcloud.com/:8000');
+        var myIoSocket = io.connect('http://nodejs-igrica.rhcloud.com/:8000', {'forceNew':true });
 
         mySocket = socketFactory({
             ioSocket: myIoSocket
