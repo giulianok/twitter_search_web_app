38dcfd11df6363463e8e9fc1f970dcc13a420619
isBatak <ivicabatinic123@gmail.com>
2015-05-28 01:50:48 +0200

    change to production url for socket

--- public/app/services/socketService.js
+++ public/app/services/socketService.js
@@ -1,7 +1,7 @@
 app.
     factory('socket', ['socketFactory', function(socketFactory) {
-        var myIoSocket = io.connect('http://localhost:3000');
-        //var myIoSocket = io.connect('http://nodejs-igrica.rhcloud.com/:8000', {'forceNew':true });
+        //var myIoSocket = io.connect('http://localhost:3000');
+        var myIoSocket = io.connect('http://nodejs-igrica.rhcloud.com/:8000', {'forceNew':true });
 
         mySocket = socketFactory({
             ioSocket: myIoSocket
