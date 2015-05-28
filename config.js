a78bc1a2b23291ddff4ceb714a310dc95ba93f61
isBatak <ivicabatinic123@gmail.com>
2015-05-28 02:13:04 +0200

    authenticate against another database

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var mongo = require('mongoskin');
 
-var db_url = process.env.OPENSHIFT_MONGODB_DB_URL || "mongodb://localhost:27017/";
+var db_url = process.env.OPENSHIFT_MONGODB_DB_URL + 'twittersearch?authSource=nodejs'|| 'mongodb://localhost:27017/twittersearch';
 
-exports.db = mongo.db( db_url + "twittersearch", {native_parser:true});
+exports.db = mongo.db( db_url, {native_parser:true});
