2d10fd79836681472368aaf6b838305a5a953a19
isBatak <ivicabatinic123@gmail.com>
2015-05-28 02:16:35 +0200

    database name changed

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var mongo = require('mongoskin');
 
-var db_url = process.env.OPENSHIFT_MONGODB_DB_URL + 'twittersearch?authSource=nodejs'|| 'mongodb://localhost:27017/twittersearch';
+var db_url = process.env.OPENSHIFT_MONGODB_DB_URL + 'nodejsgit s'|| 'mongodb://localhost:27017/twittersearch';
 
 exports.db = mongo.db( db_url, {native_parser:true});
