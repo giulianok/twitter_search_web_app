522f77d85dd2dcd7a80441480034aa94803ca664
isBatak <ivicabatinic123@gmail.com>
2015-06-01 00:54:10 +0200

    highlight filter: escape special characters when building regexp query

--- public/app/filters/highlightFilter.js
+++ public/app/filters/highlightFilter.js
@@ -1,7 +1,9 @@
 app.filter('highlight', function($sce) {
         return function(text, phrase) {
             if (phrase) {
-                phrase = phrase.trim().replace(/\s+/g, '|');
+                phrase = phrase.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1"); //escape special characters
+                phrase = phrase.trim();                                         //trim whitespaces from start and end of the string
+                phrase = phrase.replace(/\s+/g, '|');                           //replace spaces with |
                 text = text.replace(new RegExp('(^|\\W)('+ phrase +')(?=\\W|$)', 'gi'), '$1<span class="highlighted">$2</span>');
             }
 
