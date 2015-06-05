ea92cdd370081d907b084f70394dac6131001512
isBatak <ivicabatinic123@gmail.com>
2015-06-05 16:29:21 +0200

    last update

--- config/twit.js
+++ config/twit.js
@@ -1,9 +1,9 @@
 var Twit = require('twit');
 var T = new Twit({
-    consumer_key: 'HsHtIE2NsH4ga4CPEQVp3wfxg',
-    consumer_secret: 'qZnEyf2EykW3bs8D3nXgomdM0I4P45Criz5jY0qjarWnaSEZAz',
-    access_token: '777919578-xMlu4yNNuRZXqDJCQEOT0PJuancNKrEOx83GQwQD',
-    access_token_secret: '5PTPJAuIH8vuR44i4TSopAqq4rQzg7u8Ycguvcy6T1RpZ'
+    consumer_key: 'YOUR-CONSUMER-KEY',
+    consumer_secret: 'YOUR-CONSUMER-SECRET',
+    access_token: 'YOUR-ACCESS-TOKEN',
+    access_token_secret: 'YOUR-ACCESS-TOKEN-SECRET'
 });
 
 module.exports.T =  T;
